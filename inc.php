620ec54f3e6adaa6181336680e3cae0c4aeacadc
David Gillies <daveg@dorja.com>
2012-01-20 06:25:36 +1100

    Cleaned up inc.php

--- inc.php
+++ inc.php
@@ -8,7 +8,8 @@
 
 # The path to your Nagios config CGI config. 
 $cgi_ini='/etc/nagios3/cgi.cfg';
-$nagios_home='http://mythtv.local/nagios/';
+# URL of your Nagios installation
+# $nagios_home='http://nagios.server.url/nagios/';
 
 # Then use the above to get the path to the main config file. 
 $nagios_cfg = ini_getitem($cgi_ini, "main_config_file");
